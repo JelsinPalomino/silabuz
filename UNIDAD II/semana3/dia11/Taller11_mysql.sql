aff976fa9781ac8b16a16a6b4ca033cc6d96fd54
JelsinPalomino <jstpalomino@hotmail.com>
2022-11-02 22:06:04 -0500

    Clase de bases de datos

---
+++ UNIDAD II/semana3/dia11/Taller11_mysql.sql
@@ -0,0 +1,7 @@
+show tables;
+insert into cliente (idcliente, nombre, correo) value (1, 'Pepito', 'pepito@gmail.com');
+select * from cliente;
+
+create table notas(nombre varchar(10));
+
+drop table notas
